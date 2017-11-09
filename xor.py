fb5f86f2c2b82fa08661fe23d8f347847d40ea4c
Larry Lu <pudding850806@gmail.com>
2017-11-09 11:57:23 +0800

    Fix bug in xor.py

--- xor.py
+++ xor.py
@@ -16,14 +16,14 @@ target = np.array(labels, np.bool_)
 feature_columns = [tf.contrib.layers.real_valued_column("")]
 
 learning_rate = 0.1
-epoch = 1
+epoch = 1e4
 
 estimator = tf.contrib.learn.SKCompat(
     tf.contrib.learn.DNNClassifier(
         hidden_units=[3],
         feature_columns=feature_columns,
-        optimizer=tf.train.GradientDescentOptimizer(learning_rate)
-        activation_fn=tf.nn.sigmoid,
+        optimizer=tf.train.GradientDescentOptimizer(learning_rate),
+        activation_fn=tf.nn.sigmoid
     )
 )
 
